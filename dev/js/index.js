22797b1444ac65ccfba56e23537b83b91af25ad1
Igor <gg.ziplant@gmail.com>
2020-07-20 00:35:22 +0300

    export module test

--- dev/js/index.js
+++ dev/js/index.js
@@ -4,3 +4,9 @@ import { Calc } from './lib/calc'
 Node.prototype.calc = function() {
   new Calc(this)
 }
+
+exports.calc = function() {
+  Node.prototype.calc = function() {
+    new Calc(this)
+  }
+}
